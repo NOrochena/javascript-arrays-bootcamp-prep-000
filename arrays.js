18a72c9f4e30f648c5861d2382dbc00fab54ed77
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-26 00:29:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(arr, ele){
-  
+  return arr.unshift(ele)  
 }
 
 function destructivelyAddElementToEndOfArray(arr, ele){
